9b7df7084817b9f3730c4be8ed19bb7db9f3092e
ningyuanyueshao <1357188405@qq.com>
2023-03-09 10:01:44 +0800

    revise something

--- src/main/java/scms/Dao/SCMSFILE.java
+++ src/main/java/scms/Dao/SCMSFILE.java
@@ -14,34 +14,35 @@ import java.io.IOException;
 
 public class SCMSFILE {
 //    相对路径
-    private String OS = "D:\\SCMSFILE\\";
+    private static final String OS = "D:\\SCMSFILE\\";
     //班级文件夹
     public File classDirectory;
-
-    //静态文件？？？？
+    //学生学号文件夹
+    public File studentDirectory;
+    //静态文件？？？？干嘛的
     public File assertData;
     // 课程文件，直接在班级文件夹下
-
-    //public File studentDirectory; 学生文件夹，不知道添了后其他代码哪里要改？？？？下面这个构造方法肯定要，那其他地方读删文件的地方应该也要
     public File courseData;
-    //    用户数据 - 名称,密码,注册时间
+    //用户数据 - 名称,密码,注册时间 ,在学号文件夹下
     public File userData;
-    //    文件哈希
+    //活动文件，在学号文件夹下
+    public File activityData;
+    //文件哈希？？？？干嘛的
     public File hashData;
-    //    构造函数
+    //构造函数
     public SCMSFILE(String className,String userName) {
-        //pathname传的是用户名
         this.classDirectory = new File(OS + className);
-        this.assertData = new File(OS+className+"/assert");
-        this.courseData = new File(OS+className+"课程文件名");
-        this.userData = new File(OS + className+"/userdata.scms");
-        this.hashData = new File(OS + className+"/hashdata.scms");
+        this.courseData = new File(OS+className+"/courseData.scms");
+        this.studentDirectory = new File(OS + className + userName);
+        this.assertData = new File(OS+className + userName + "/assert");
+        this.userData = new File(OS + className + userName + "/userData.scms");
+        this.activityData = new File(OS + className + userName + "/activityData.scms");
+        this.hashData = new File(OS + className+ userName +"/hashdata.scms");
     }
 
 
     public boolean exists(){
-        if(this.classDirectory.exists()==true)return true;
-        else return false;
+        return this.classDirectory.exists();
     }
     public  boolean creat() throws IOException {
         if(this.classDirectory.mkdirs()&&this.userData.createNewFile()&&this.courseData.createNewFile()&&this.assertData.mkdirs()){
