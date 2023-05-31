f922d7b82d330f640c2d4b2f12d879034569edd4
ningyuanyueshao <1357188405@qq.com>
2023-06-01 00:40:49 +0800

    添加闹钟日志相关

--- src/main/java/scms/Dao/FunctionMatch.java
+++ src/main/java/scms/Dao/FunctionMatch.java
@@ -13,6 +13,7 @@ public class FunctionMatch {
             case "rename":return "更改姓名";
             case "changePersonalWord":return "更改个性签名";
             case "AddItem": return "添加数据信息";
+            case "AddAlarm": return "添加闹钟";
             case "DeleteItem":return "删除数据信息";
             case "NoticeProcessor": return "同意通知";
             case "NoticeRemover":return "拒绝通知";
